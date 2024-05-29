5229db15f1c68e7d8b9843a09036be2baeaf8007
manglij <nicolas.lepage41@gmail.com>
2024-05-29 08:19:16 +0200

    centre hollywod fini + debut des docstring

--- requetes.py
+++ requetes.py
@@ -1,55 +1,42 @@
 import networkx as nx
+import json
 import matplotlib.pyplot as plt
 import re
+import time
 
+def json_vers_nx(nom_fichier):
+    """_summary_
 
+    Args:
+        nom_fichier (_type_): _description_
 
-chemin_fichier = "jeux de données réduits-20240507/data_10000.txt"
-fic = open("dataTest.txt", "a")
-Hollywood = nx.Graph()
+    Returns:
+        _type_: _description_
+    """
+    Hollywood = nx.Graph()
 
-cpt = sum(1 for _ in chemin_fichier)
+    with open(nom_fichier, 'r', encoding="utf-8") as f:
+        for line in f:
+            tests = json.loads(line)  # Use json.loads instead of eval
 
-with open(chemin_fichier, 'r') as f:
-    for i in range(cpt):
-        test = f.readline()
-        tests = eval(test)
+            # Create a set of actors
+            actors = {re.sub(r"[\[\]]", "", actor).split("|")[-1] for actor in tests["cast"]}
+            
+            # Add nodes (actors) to the graph
+            Hollywood.add_nodes_from(actors)
 
+            # Add edges between every pair of actors in the cast
+            for actor in actors:
+                for other_actor in actors:
+                    if actor != other_actor:
+                        Hollywood.add_edge(actor, other_actor)
 
-        for acteurs in tests["cast"]:
-            string = re.sub("[[]", "", acteurs)
-            s1 = re.sub("[]]", "", string)
-            if "|" in s1:
-                stock = s1.split("|")
-                stringAc = stock[1]
-            else:
-                stringAc = s1
-            Hollywood.add_node(stringAc)
+    return Hollywood
 
-
-        for acteur in tests["cast"]:
-            for lien in tests["cast"]:
-                if acteur != lien:
-                    stringAc = re.sub("[[]", "", acteur)
-                    stringAc2 = re.sub("[]]", "", stringAc)
-                    stringLi = re.sub("[[]", "", lien)
-                    stringLi2 = re.sub("[]]", "", stringLi)
-
-                    if "|" in stringAc2:
-                        stock = stringAc2.split("|")
-                        stringAcAjoute = stock[1]
-                    else:
-                        stringAcAjoute = stringAc2
-
-                    if "|" in stringLi2:
-                        stock = stringLi2.split("|")
-                        stringLiAjoute = stock[1]
-                    else:
-                        stringLiAjoute = stringLi2
-                    Hollywood.add_edge(stringAcAjoute, stringLiAjoute)
-                    #print(stringAc, stringLi)
-
-fic.close()
+start = time.time()
+Hollywood = json_vers_nx("jeux de données réduits-20240507/data.txt")
+end = time.time()
+print(end - start)
 
 
 def collaborateurs_communs(G,u,v):
@@ -58,7 +45,7 @@ def collaborateurs_communs(G,u,v):
     Parametres:
         G: le graphe
         u: le sommet de départ
-        v: le sommet de départ
+        v: le sommet d'arriver
 
     """
     if u not in G.nodes or v not in G.nodes:
@@ -95,10 +82,29 @@ def collaborateurs_proches(G,u,k):
     return collaborateurs
 
 def est_proche(G,u,v,k=1):
+    """Fonction renvoyant True si u et v sont voisine. La fonction renvoie None si u ou v est absent du graphe.
+    
+
+    Args:
+        G: le graphe
+        u: le sommet de départ
+        v: le sommet d'arriver
+        k:la distance depuis u Defaults to 1
+    """
     return v in collaborateurs_proches(G,u,k)
 
 
 def distance(G,u,v):
+    """_summary_
+
+    Args:
+        G: le graphe
+        u: le sommet de départ
+        v: le sommet d'arriver
+
+    Returns:
+        _type_: _description_
+    """
     if u not in G.nodes or v not in G.nodes:
         print(u, 'ou',v,"sont des illustres inconnus")
         return None
@@ -124,21 +130,69 @@ print(collaborateurs_proches(Hollywood,"James Mapes",1))
 
 print(distance(Hollywood,"James Mapes","Holly Hunter"))
 
+
 def centralite(G,u):
+    """_summary_
+
+    Args:
+        G: le graphe
+        u: le sommet de départ
+
+    Returns:
+        _type_: _description_
+    """
     if u not in G.nodes:
         print(u,"est un illustre inconnu")
         return None
+    deja_vue = set()
+    deja_vue.add(u)
     collaborateurs = set()
     collaborateurs.add(u)
     for i in range(len(G.nodes)):
         collaborateurs_directs = set()
         for c in collaborateurs:
             for voisin in G.adj[c]:
-                if voisin not in collaborateurs:
+                if voisin not in deja_vue:
                     collaborateurs_directs.add(voisin)
         if len(collaborateurs_directs)==0:
-            return i    
-        collaborateurs = collaborateurs.union(collaborateurs_directs)
+            return i
+        collaborateurs = collaborateurs_directs
+        deja_vue = deja_vue.union(collaborateurs_directs)
     return None
 
 
+
+def centre_hollywood(G):
+    """_summary_
+
+    Args:
+        G: le graph
+
+    Returns:
+        _type_: _description_
+    """
+    name = ""
+    minimum = None
+    for node in G.nodes():
+        if len(G.adj[node]) > 1:
+            stock = centralite(G, node)
+            if minimum is None or stock < minimum:
+                name = node
+                minimum = stock
+    return name
+
+
+
+
+
+start = time.time()
+print(centralite(Hollywood,"Leonardo DiCaprio"))
+end = time.time()
+print(end-start)
+
+
+
+start = time.time()
+#print(centralite_hollywood(Hollywood))
+end = time.time()
+print(end-start)
